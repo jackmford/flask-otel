26c3476453be8a897d7434dc72bb19815c819a76
jackmford <jackmitchellfordyce@gmail.com>
2023-02-24 14:11:21 -0600

    feat: inspecting things in the console

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ import requests
 from flask import Flask
 app = Flask(__name__)
 
-app.route("/")
+@app.route("/")
 def main():
     response = requests.get('https://jackmitchellfordyce.com')
     print(response)
 
-    return response.status_code
+    return str(response.status_code)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='8080')
