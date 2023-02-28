76a9e92176ad3b2b210b296e23e18a01aaa72900
jackmford <jackmitchellfordyce@gmail.com>
2023-02-28 14:51:11 -0600

    feat: first manual instrumentation

--- server.py
+++ server.py
@@ -1,11 +1,22 @@
 import random
 import requests
 from flask import Flask
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+
 app = Flask(__name__)
+provider = TracerProvider()
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(__name__)
 
 @app.route("/roll")
 def dyce_roll():
-    num = random.randint(1,6)
+    with tracer.start_as_current_span("random_roll"):  
+        span = trace.get_current_span()
+        num = random.randint(1,6)
+        span.add_event( "log", {
+            "num": num,
+        })
     print(num)
 
     return str(num)
